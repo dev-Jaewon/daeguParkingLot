525673274c031e240e5f7ef98ea6608823adc711
dev-Jaewon <app235naver@gmail.com>
2023-12-08 18:49:39 +0900

    modified 스트릭 모드 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,40 @@
-import './App.css'
+import { useRef, useEffect } from 'react';
+import styled from '@emotion/styled';
+import { getParkList } from './utils/api';
+import { useMap } from './hooks/useMap';
 
 function App() {
+  const mapElement = useRef<HTMLDivElement | null>(null);
+
+  const { center, setMarker } = useMap({ targetEle: mapElement });
+
+  useEffect(() => {
+    console.log(123)
+    const call = async () => {
+      const result = await getParkList({ lat: center.lat, lot: center.lot, radius: 10 });
+
+      setMarker(result.data?.items.item);
+    }
+
+    call()
+  }, [center]);
 
   return (
-    <>
-    </>
+    <Container>
+      <Map ref={mapElement}>
+      </Map>
+    </Container>
   )
 }
 
-export default App
+export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Map = styled.div`
+  width: 100%;
+  height: 100%;
+`;
