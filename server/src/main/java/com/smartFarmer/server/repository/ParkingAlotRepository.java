fb41cf4410f9e735eddf1a98e61d7d85844d1c31
dev-Jaewon <app235naver@gmail.com>
2023-12-10 15:17:41 +0900

    modified 파일 위치 이동

--- server/src/main/java/com/smartFarmer/server/repository/ParkingAlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartFarmer.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smartFarmer.server.entity.ParkingAlotEntity;
-
-public interface ParkingAlotRepository extends JpaRepository<ParkingAlotEntity, Long> {
-    
-}
