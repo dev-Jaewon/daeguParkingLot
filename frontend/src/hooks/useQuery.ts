dd44cd375b9fe3d05ebfcd3ebb22a3d25712f2fc
dev-Jaewon <app235naver@gmail.com>
2023-12-18 23:18:27 +0900

    feat api hook 추가

---
+++ frontend/src/hooks/useQuery.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+import { ResponseSearchList } from '../types/ResponseSearchList';
+import { getSearch } from '../utils/api';
+import { SearchParkList } from '../types/SearchParkList';
+
+export const useSearchQuery = (queryParam: SearchParkList) => {
+    const { data, isLoading, isFetching } = useQuery<ResponseSearchList>({
+        queryKey: ['markers', queryParam], queryFn: () => getSearch(queryParam),placeholderData: previousData => previousData
+    });
+
+    return {data, isLoading, isFetching}
+}
