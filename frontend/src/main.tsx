da5eedca81c0385ae4148ee10811dc5158ff5a0f
dev-Jaewon <app235naver@gmail.com>
2023-12-19 08:56:02 +0900

    feat react router set

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,31 @@
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
 import App from './App.tsx'
+
 import './styles/index.scss'
 import './styles/common.scss'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Auth } from './pages/auth.tsx';
 
 const queryClient = new QueryClient();
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+        path: "/auth",
+        element: <Auth />,
+      },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-        <App />
+         <RouterProvider router={router} />
     </QueryClientProvider>
 );
